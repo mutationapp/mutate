94ab1fb597ab91d3cf735c44d10edd0ba67c4689
ibrahim velinov <ibsukru@gmail.com>
2020-08-09 20:32:49 +0200

    🚀

--- stryker.conf.js
+++ stryker.conf.js
@@ -8,7 +8,8 @@ const { files, mutate, page } = getMutationCandidates()
 console.log('MUTATION CANDIDATES\n', mutate)
 
 module.exports = {
-  files: files.length ? ['jest/**/*.js', '**/shared/*.js', ...files] : [],
+  files:
+    files && files.length ? ['jest/**/*.js', '**/shared/*.js', ...files] : [],
   mutate,
   mutator: 'javascript',
   packageManager: 'yarn',
